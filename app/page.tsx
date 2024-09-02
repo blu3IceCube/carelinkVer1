4bf154411ead1c19d7f6f5f9af347ab86ab0ee25
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-09-02 18:01:12 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,44 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[500px]">
+          <div className="flex">
+            <Image
+              src="/assets/icons/logo-icon.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-10 w-fit"
+            />
+            <span className="text-3xl font-semibold ml-3">CareLink</span>
+          </div>
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CareLink
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        width={1000}
+        height={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
+}
